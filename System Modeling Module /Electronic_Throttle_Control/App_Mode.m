f382d1a82c94da58e19773e6c716e87ddbc71d5d
yassa <123013528+yassa404@users.noreply.github.com>
2024-07-16 00:19:05 +0300

    Add files via upload

---
+++ System Modeling Module /Electronic_Throttle_Control/App_Mode.m
@@ -0,0 +1,8 @@
+classdef (Enumeration) App_Mode < Simulink.IntEnumType
+    enumeration
+        normal(0)
+        Downgraded_Sen_1(1)
+        Downgraded_Sen_2(2)
+        Failure(3)
+    end    
+end
