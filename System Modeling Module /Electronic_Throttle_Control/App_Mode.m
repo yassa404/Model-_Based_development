dd4c37c4dd1ceb73d2ae98403182b58de1d44fd3
yassa <123013528+yassa404@users.noreply.github.com>
2024-08-05 17:10:01 +0300

    Delete System Modeling Module  directory

--- System Modeling Module /Electronic_Throttle_Control/App_Mode.m
+++
@@ -1,8 +0,0 @@
-classdef (Enumeration) App_Mode < Simulink.IntEnumType
-    enumeration
-        normal(0)
-        Downgraded_Sen_1(1)
-        Downgraded_Sen_2(2)
-        Failure(3)
-    end    
-end
